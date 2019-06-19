75dd6c145c3bb56e83f2a6ff1d81716e5b2f44fe
oscar ugarte <oscar.ugrt@gmail.com>
2019-06-19 12:23:50 -0300

    Creada la entidad Escribania.

---
+++ src/Entity/Escribania.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EscribaniaRepository")
+ */
+class Escribania
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $direccion;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $telefono;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $foto;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=8)
+     */
+    private $latitud;
+
+    /**
+     * @ORM\Column(type="decimal", precision=11, scale=8)
+     */
+    private $longitud;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $estado;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDireccion(): ?string
+    {
+        return $this->direccion;
+    }
+
+    public function setDireccion(string $direccion): self
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?int
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(int $telefono): self
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getFoto(): ?string
+    {
+        return $this->foto;
+    }
+
+    public function setFoto(string $foto): self
+    {
+        $this->foto = $foto;
+
+        return $this;
+    }
+
+    public function getLatitud()
+    {
+        return $this->latitud;
+    }
+
+    public function setLatitud($latitud): self
+    {
+        $this->latitud = $latitud;
+
+        return $this;
+    }
+
+    public function getLongitud()
+    {
+        return $this->longitud;
+    }
+
+    public function setLongitud($longitud): self
+    {
+        $this->longitud = $longitud;
+
+        return $this;
+    }
+
+    public function getEstado(): ?bool
+    {
+        return $this->estado;
+    }
+
+    public function setEstado(bool $estado): self
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+}
